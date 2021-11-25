9fad958333f50ecfcdf46e6e2b770c9d222ad89b
Lars Martin <lars.martin@tum.de>
2021-11-25 14:12:53 -0500

    Evaluate list, write details

--- logger.py
+++ logger.py
@@ -1,13 +1,13 @@
 
-from model import Model
-    
 
 class Logger():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name) -> None:
+        self.details = open(f"{name}-details.csv",'a')
+        self.details.write(f'Question, Answer, Guess, Label')
 
-    def append_details_csv(question: str, answer: str, guess: str, label: str):
+    def append_details_csv(self, question: str, answer: str, guess: str, label: str):
         "append paramters for each question in the Synonym Test dataset, in a single line"
+        self.details.write(f'\n{question}, {answer}, {guess}, {label}')
 
-    def append_analysis_csv(params: Model):
-        "append model parameters in single line"
+    # def append_analysis_csv(self, params: Model):
+    #     "append model parameters in single line"
--- model.py
+++ model.py
@@ -1,15 +1,23 @@
 import gensim.downloader as api
 import gensim.utils
 import os.path
+import numpy as np
+import gensim.similarities
+import random
+import logger
+
+random.seed(42)
+
 class Model():
     def __init__(self, name:str) -> None:
         self.name = name
-        C = 0   # number of correct labels
-        V = 0   # number of questions answered w/o guessing
+        self.C = 0          # number of correct labels
+        self.V = 0          # number of questions answered w/o guessing
         self.download()
+        self.l = logger.Logger(name)
      
     def download(self) -> None:
-        print("\nDownloading")
+        print(f"\nDownloading {self.name}")
         if not os.path.exists(f'data/{self.name}.d2v'):
             self.model = api.load(self.name)
             self.model.save(f'data/{self.name}.d2v')
@@ -18,5 +26,23 @@ class Model():
         self.voc = len(self.model)
         return
 
-    def predict(self, dataset) -> None:
-        print("\npredict")
+    def evaluate(self, dataset: np.ndarray, feature_len: int) -> None:
+        cosines = np.empty((len(dataset),feature_len)) # vector to store cosine values for each possible synonym
+        print(type(self.model))
+        for i in range(len(dataset)):
+            for j in range(feature_len):
+                try: 
+                    cosines[i,j] = round(self.model.similarity(dataset[i,0], dataset[i,2+j]),4)
+                except Exception as e:
+                    cosines[i,j] = -1   # guess probabilty later if word is unseen 
+                    pass                # will result in 4x -1 if guess word raises exception
+            if all([round(x,4) < 0 for x in cosines[i,:]]):
+                idx_guess = random.randint(0,3)
+                label = 'guess'
+            else:
+                idx_guess = np.argmax(cosines[i,:])
+                if dataset[i, idx_guess+2] == dataset[i, 1]:
+                    label = 'correct'
+                else:
+                    label = 'wrong'
+            self.l.append_details_csv(dataset[i,0], dataset[i,1], dataset[i, idx_guess+2], label)
