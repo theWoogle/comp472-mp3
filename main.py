27beabf844ca7fd2072ed01674d80653d77f91d8
carlt14 <carltuquero14@gmail.com>
2021-11-25 23:57:52 -0500

    Add 4 other pretrained models

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ from logger import Logger
 if __name__ == "__main__":
 
     # Download models --> Takes some minutes
-    google_300 = model.Model('word2vec-google-news-300')    
+    google_300 = model.Model('word2vec-google-news-300')
+
+    #Task2
+    #2 new models from different corpora
+    twitter_200 = model.Model('glove-twitter-200')
+    wiki_200 = model.Model('glove-wiki-gigaword-200')
+    #2 new models from the same corpus but different embedding sizes
+    twitter_100 = model.Model('glove-twitter-100')
+    wiki_100 = model.Model('glove-wiki-gigaword-100')
+
 
     # Download datasets
     synonyms = np.loadtxt('data/synonyms.csv', dtype=str, delimiter=',', skiprows=1)
@@ -13,8 +22,14 @@ if __name__ == "__main__":
     # Evaluate model 1 word2vec-google-news-300
     google_300.evaluate(synonyms, 4)
 
-    # Evaluate model 2 e.g. twitter-300
-    # Evaluate model 3 e.g. gigaword-300
-    # Evaluate model 4 e.g. wikipedia-300
-    # Evaluate model 5 e.g. wikipedia-1024
+    #Task2
+    # Evaluate model 2  glove-twitter-200
+    twitter_200.evaluate(synonyms,4)
+    # Evaluate model 3 glove-wiki-gigaword-200
+    wiki_200.evaluate(synonyms,4)
+    # Evaluate model 4 glove-twitter-100
+    twitter_100.evaluate(synonyms,4)
+    # Evaluate model 5 glove-wiki-gigaword-100
+    wiki_100.evaluate(synonyms,4)
+
     # Plot results
