210d8978e0a557f369d15b2e6e849b652d398249
Lars Martin <lars.martin@tum.de>
2021-12-08 11:45:14 -0500

    formatting, add /results

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import model
 import numpy as np
-from logger import Logger
-import utils
+import plotter
 import numpy as np
 
 if __name__ == "__main__":
 
-    # Download datasets
-    synonyms = np.loadtxt('data/synonyms.csv', dtype=str, delimiter=',', skiprows=1)
-
-
-    # model_names = ['word2vec-google-news-300', 'glove-twitter-200', 'glove-wiki-gigaword-200',
-    #                'glove-twitter-100', 'glove-wiki-gigaword-100']
+    # Download dataset
+    synonyms = np.loadtxt('data/synonyms.csv', dtype=str,
+                          delimiter=',', skiprows=1)
 
     model_names = ['word2vec-google-news-300', 'glove-twitter-200', 'glove-wiki-gigaword-200',
                    'glove-twitter-100', 'glove-twitter-50']
 
     models = []
-    models_accuracy=[]
+    models_accuracy = []
     for name in model_names:
         models.append(model.Model(name))
+    
+    print(f"Evaluating Models")
     for model in models:
-        accuracy = model.evaluate(synonyms,4)
-        models_accuracy.append(np.array(accuracy))
+        accuracy = model.evaluate(synonyms, 4)
+        models_accuracy.append(accuracy)
 
-    # models_n = ['google300', 'twitter200', 'wiki200', 'twitter100', 'wiki100','humangs','randb']
+    models_n = ['google300', 'twitter200', 'wiki200',
+                'twitter100', 'twitter50', 'humangs', 'randb']
 
-    models_n = ['google300', 'twitter200', 'wiki200', 'twitter100', 'twitter50', 'humangs', 'randb']
+    # Human gold standard from moodle
     models_accuracy.append(0.855)
+    # random guessing 1/4
     models_accuracy.append(0.25)
-    utils.Plotter(models_n, models_accuracy)
-    print(models_n)
-    print(models_accuracy)
+
+    plotter.Plotter(models_n, models_accuracy)
+
+    print(f"Models: \t{models_n}")
+    print(f"accuracies: \t{models_accuracy}")
